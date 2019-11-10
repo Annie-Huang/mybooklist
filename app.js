076a73e2639c0064ea2a1e40f706f6c87e227a16
Annie Huang <annie.hn@gmail.com>
2019-11-10 13:09:57 +1100

    Add #book-form submit event listener for user enters new book info into the form.

--- app.js
+++ app.js
@@ -48,5 +48,21 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', e => {
+    // Prevent actual submit
+    e.preventDefault();
+
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Instatliate book
+    const book = new Book(title, author, isbn);
+
+    // Add Book to UI
+    UI.addBookToList(book);
+});
+
 
 // Event: Remove a Book
